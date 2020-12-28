b715eab1cec3654cc20f2ac455cd6684a5c0a461
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2020-12-28 13:31:28 -0500

    Inicio

---
+++ 01_VentanaGrafica.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+dave = turtle.Turtle()
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
+
+turtle.mainloop()
