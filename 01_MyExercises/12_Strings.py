0adcb08c04f8352f7980c739bf37ba59c086d0b6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2020-12-31 18:19:44 -0500

    tree

---
+++ 01_MyExercises/12_Strings.py
@@ -0,0 +1,9 @@
+def main():
+    s = 'hola'
+
+    r = 'l' + s[1:]
+
+    print(r)
+
+if __name__ == '__main__':
+    main()
---
+++ 01_MyExercises/15_Tree.py
@@ -0,0 +1,55 @@
+import turtle,random
+
+w = turtle.Screen()
+t = turtle.Turtle()
+t.color("green")
+numNivel = 13
+largoRama = 120
+angulo = 17
+
+def cambioColor():
+    colors=['blue' , 'red', 'orange', 'purple']
+    t.pencolor(random.choice(colors))
+
+def dibujaArbol(largoRama, Nivel):
+
+    cambioColor()
+    width = t.width()  
+
+    t.width(width * 3.0 / 4.0)  
+
+    largoRama = 3.0 / 4.0 * largoRama
+
+    t.left(angulo)
+    t.forward(largoRama)
+
+    if Nivel < numNivel:
+        dibujaArbol(largoRama, Nivel + 1)
+    t.back(largoRama)
+    t.right(2 * angulo)
+    t.forward(largoRama)
+
+    if Nivel < numNivel:
+        dibujaArbol(largoRama, Nivel + 1)
+    t.back(largoRama)
+    t.left(angulo)
+
+    t.width(width)  
+	
+def main():
+	t.speed("fastest")
+	
+	t.left(90)
+	t.width(numNivel) #
+	t.penup()
+	t.back(largoRama)
+		
+	t.pendown()
+	t.forward(largoRama)
+
+	dibujaArbol(largoRama, 2)
+
+	turtle.done()
+
+if __name__ == "__main__":
+	main()
