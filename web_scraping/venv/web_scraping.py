09fe4dbcd25ae577d8c3340debdb46a0a2139bd9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-11 22:12:53 -0500

    Bunny Girl Senpai

--- web_scraping/venv/web_scraping.py
+++ web_scraping/venv/web_scraping.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib
+import urllib.request
 
 
 
 def run():
-    for i in range(1, 1501):
+    for i in range(1, 2409):
         response = requests.get('https://xkcd.com/{}'.format(i))
         soup = BeautifulSoup(response.content, 'html.parser')
         image_container = soup.find(id='comic')
@@ -13,7 +13,7 @@ def run():
         image_url = image_container.find('img')['src']
         image_name = image_url.split('/')[-1]
         print('Descargando la imagen {}'.format(image_name))
-        urllib.urlretrieve('https:{}'.format(image_url), image_name)
+        urllib.request.urlretrieve('https:{}'.format(image_url), image_name)
 
 
 if __name__ == '__main__':
